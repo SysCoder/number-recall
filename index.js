14188125e0154f0ffe4a2ea62a664e2b22408db5
Marcellus Pelcher <pelcher@gmail.com>
2017-06-11 16:48:28 -0700

    Fix array in context issue - Reduce by one when the answer is wrong

--- index.js
+++ index.js
@@ -16,22 +16,22 @@ exports.numberRecall = (request, response) => {
     let introSpeech = "Welcome to Number Recall. Try your hand at recalling a list of numbers.  We will start with one number. The length will increase by one when you correctly repeat the sequence. The length will decrease by one when you do not repeat the sequence correctly.";
     let nextRandomNumber = Math.floor(Math.random() * 9) + 1;
     app.setContext(NUMBER_SEQUENCE, 100, {"sequence": [nextRandomNumber]});
-    app.ask(introSpeech + " Let's started with: "  + nextRandomNumber);
+    app.ask(introSpeech + " Let's start with: "  + nextRandomNumber);
   }
 
   function attemptedSequence(app) {
     let numberSequenceUttered = app.getArgument("NumberSequence").match(/\d/g).map(num => parseInt(num));
     let numberSequencedAskFor = app.getContextArgument(NUMBER_SEQUENCE, "sequence").value;
+
     if (numberSequenceUttered.toString() === numberSequencedAskFor.toString()) {
       let nextRandomNumber = Math.floor(Math.random() * 9) + 1;
       numberSequencedAskFor.push(nextRandomNumber);
-      app.setContext(NUMBER_SEQUENCE, 100, {"sequence": [numberSequencedAskFor]});
-      app.ask("That is correct! " + numberSequencedAskFor);
+      app.setContext(NUMBER_SEQUENCE, 100, {"sequence": numberSequencedAskFor});
+      app.ask("That is correct! ... " + numberSequencedAskFor);
     } else {
-      let nextRandomNumber = Math.floor(Math.random() * 9) + 1;
-      app.setContext(NUMBER_SEQUENCE, 100, {"sequence": [nextRandomNumber]});
-      app.ask(introSpeech + " Let's started with: "  + nextRandomNumber);
-      app.ask("You said: " + numberSequenceUttered + ". That is not correct.");
+      numberSequencedAskFor.pop();
+      app.setContext(NUMBER_SEQUENCE, 100, {"sequence": numberSequencedAskFor});
+      app.ask("You said: " + numberSequenceUttered + ". That is not correct. " + numberSequencedAskFor);
     }
   }
 
